18df1da8f180f76052d9f7b10b002924f97f7592
fullstackdev2020 <fullstackdeveloper2020@outlook.com>
2020-04-07 12:56:46 +0500

    update

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -10,7 +10,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         render json: [user], status: :ok
       else
         session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) 
-        # User.create(session['devise.google_data'])
+        User.create(session['devise.google_data'])
         render json: user.errors
       end
     end
@@ -27,7 +27,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         render json: user
       else
         session["devise.facebook_data"] = request.env["omniauth.auth"]
-        # User.create(session['devise.facebook_data'])
+        User.create(session['devise.facebook_data'])
         render json: user.errors
       end
     end
